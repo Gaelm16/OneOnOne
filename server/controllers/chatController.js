c76bb732afa0419a31838592294efeb21d65f51c
Gaelm16 <sandro@Sandros-iMac.com>
2022-08-29 21:45:21 -0400

    created new group controllers

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -39,7 +39,7 @@ const accessChat = async (req, res) => {
 
             const fullChat = await Chat.findOne({_id: createdChat}).populate(
                 "users",
-                '-password'
+                '-passWord'
             )
 
             res.status(200).json(fullChat)
@@ -51,4 +51,58 @@ const accessChat = async (req, res) => {
 
 }
 
-module.exports = {accessChat}
+const fetchMyChats = async (req, res) =>{
+
+    try{
+        Chat.find({users: {$elemMatch: {$eq: req.user._id}}})
+            .populate("users", "-passWord")
+            //.populate("groupAdmin", "-passWord")
+            .populate("lastestMessage")
+            .sort({updatedAt: -1})
+            .then(async (results) => {
+                results = await User.populate(results, {
+                    path: "latestMessage.sender",
+                    select: "userName"
+                })
+            })
+
+            res.status(200).send(results)
+    } catch(e){
+        res.status(400)
+        console.log(e)
+    }
+   
+}
+
+const createGroupChat = async(req, res) => {
+    if(!req.body.users || !req.body.userName){
+        return res.status(400).send({"message": "please fill all missing fields"})
+    }
+
+    if(users.length < 2){
+        return res.status(400).send({"message": "there needs to be more than 2 people"})
+    }
+
+    users.push(req.userName)
+
+    try{
+        const newGroup = new Chat({
+            chatName: req.body.userName,
+            users: users,
+            isgroupChat: true,
+
+        })
+
+        const group = await Chat.findOne({_id: newGroup._id})
+            .populate("users", "-passWord")
+            .populate("groupAdmin", "-passWord")
+
+            res.status(200).send(group)
+
+    }catch(e){
+        res.status(400)
+        console.log(e)
+    }
+}
+
+module.exports = {accessChat, fetchMyChats, createGroupChat}

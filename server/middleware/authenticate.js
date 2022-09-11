54bc26993d79304aeaee502356413a2a42c45ad4
Gaelm16 <sandro@Sandros-iMac.com>
2022-09-11 16:14:24 -0400

    debugged middleware

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -2,15 +2,19 @@ const User = require('../models/userModel')
 const jwt = require("jsonwebtoken")
 
 const authenticateUser = async (req, res, next) => {
+    let token = req.cookies.token
+
     try{
-        let token = req.cookies.token
-
         const userInfo = jwt.verify(token, process.env.JWT_SECRET)
-
-        req.user = User.findById(userInfo.id)
+        req.user = userInfo.id
+        //req.user = User.findById(userInfo).select("-passWord")
         
         next()
     } catch(err){
+        res.status(401)
+    }
+
+    if(!token){
         res.status(401)
     }
 }

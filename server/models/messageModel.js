1d75eeb171b13c801251222b3c06d017418a00d1
Gaelm16 <sandro@Sandros-iMac.com>
2022-09-30 19:53:43 -0400

    no changes

--- server/models/messageModel.js
+++ server/models/messageModel.js
@@ -5,7 +5,7 @@ const messageSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'User'
     },
-    messageText: {
+    messageContent: {
         type: String, 
         required: true
     },

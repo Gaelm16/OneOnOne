0065aee7fac707b8712588b103d24a12da1702d8
Gaelm16 <sandro@Sandros-iMac.com>
2022-10-06 20:25:05 -0400

    added component

--- client/src/Components/Main/MainPage.js
+++ client/src/Components/Main/MainPage.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import MyChats from './MyChats'
 import Navbar from '../Navbar/Navbar'
+import ChatBox from './ChatBox'
 
 const MainPage = () => {
   return (
     <>
     <Navbar/>
+    <div className='main'>
     <MyChats/>
-    
+    <ChatBox/>
+    </div>
     </>
   )
 }

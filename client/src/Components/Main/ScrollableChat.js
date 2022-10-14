8f5bc43dc6ccad3b2e772732a61f7d60e13594fb
Gaelm16 <sandro@Sandros-iMac.com>
2022-10-14 19:20:07 -0400

    fixed map function

--- client/src/Components/Main/ScrollableChat.js
+++ client/src/Components/Main/ScrollableChat.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../UserContext'
 
 const ScrollableChat = ({messages}) => {
+  const {loggedIn} = useContext(AuthContext);
+
+  const isSameSender = (messages, m, i, userId) => {
+  return (
+    i < messages.length - 1 &&
+    (messages[i + 1].sender._id !== m.sender._id ||
+      messages[i + 1].sender._id === undefined) &&
+      messages[i].sender._id !== userId
+    );
+  };
+
+  const isLastMessage = (messages, i, userId) => {
+  return (
+      i === messages.length - 1 &&
+      messages[messages.length - 1].sender._id !== userId &&
+      messages[messages.length - 1].sender._id
+    );
+  };
+
   return (
     <div>
-    {messages.map((message) => {
-        <div key={message.id}>
-            
+    {messages &&
+    messages.map((message, i) => {
+      return(
+          <div key={i}>
+            {/* {(isSameSender(messages, message, i, loggedIn._id) ||
+              isLastMessage(messages, i, loggedIn._id)) && (
+                <div>{message.userName}</div>
+              )} */}
+              <span>
+                {message.messageContent}
+              </span>
         </div>
+      )
+      
     })}
     </div>
   )

ecde1663e98e4f5ad0eab7ab3ff7acc45d873ef4
Gaelm16 <sandro@Sandros-iMac.com>
2022-10-14 19:19:45 -0400

    fixed fetching

--- client/src/Components/Main/SingleChat.js
+++ client/src/Components/Main/SingleChat.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../../UserContext'
+import axios from 'axios';
+import ScrollableChat from './ScrollableChat';
 
 const SingleChat = () => {
+  const {selectedChat, setSelectedChat} = useContext(AuthContext);
+  const [messages, setMessages] = useState([]);
+
+  const fetchChatMessages = async () => {
+    if(!selectedChat) return;
+
+    try{
+      let data = await axios.get(`http://localhost:4000/api/message/${selectedChat._id}`)
+      setMessages(data.data)
+    } catch(err){
+      console.log(err)
+    }
+
+  }
+
+  useEffect(() =>{
+    fetchChatMessages();
+
+  }, [selectedChat])
+
   return (
-    <div>SingleChat</div>
+    <div>
+        SingleChat
+        {selectedChat && 
+          <ScrollableChat messages={messages}/>
+        }
+       {console.log(messages)}
+    </div>
+
   )
 }
 

0b235da313589702d85119102b3f33441e5d7e13
Gaelm16 <sandro@Sandros-iMac.com>
2022-12-22 20:40:19 -0500

    removed unused routes

--- client/src/SwitchRoute.js
+++ client/src/SwitchRoute.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from './Home'
-//import ChatsHome from './Components/Main/ChatsHome'
 import Login from './Components/LoginPage/Login'
 import Register from './Components/RegisterPage/Register'
-import MyChats from './Components/Main/MyChats'
 import MainPage from './Components/Main/MainPage'
-// import Navbar from './Components/Navbar/Navbar'
-// import Navbar from '../Navbar/Navbar'
 
 const SwitchRoute = () => {
   return (

f374ba67b5e7050d14680b74aac4da331de3df4b
Gaelm16 <sandro@Sandros-iMac.com>
2022-12-22 20:39:52 -0500

    connected socket io

--- server/server.js
+++ server/server.js
@@ -1,32 +1,51 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const userRoutes = require('./routes/userRoutes')
-const chatRoutes = require('./routes/chatRoutes')
-const messageRoutes = require('./routes/messageRoutes')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 
-dotenv.config()
+dotenv.config();
 
-mongoose.connect(process.env.DATABASE_URL, () => console.log('database connected'))
+mongoose.connect(process.env.DATABASE_URL, () => console.log('database connected'));
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(cors({
     origin: ['http://localhost:3000'],
     credentials: true,
 }));
 
-app.use('/', userRoutes)
-app.use('/api/chat', chatRoutes)
-app.use('/api/message', messageRoutes)
+const server = app.listen(5000, console.log("server started on port 5000"))
+
+const io = require('socket.io')(server, {
+    pingTimeOut: 60000,
+    cors:{
+        origin: "http://localhost:3000"
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("connected to socket io");
+
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        console.log(userData._id);
+        socket.emit("connected");
+    })
+})
+
+app.use('/', userRoutes);
+app.use('/api/chat', chatRoutes);
+app.use('/api/message', messageRoutes);
 
 app.listen(4000, () => console.log('Server is running'))

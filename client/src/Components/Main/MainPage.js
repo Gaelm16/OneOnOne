48c4fe69781233dba47959109347a5ce1c057d42
Gaelm16 <sandro@Sandros-iMac.com>
2022-12-22 20:45:11 -0500

    added search user component

--- client/src/Components/Main/MainPage.js
+++ client/src/Components/Main/MainPage.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import MyChats from './MyChats'
 import Navbar from '../Navbar/Navbar'
 import ChatBox from './ChatBox'
 import SearchUser from './SearchUser'
+import { AuthContext } from '../../UserContext'
+import Main from './Main.css'
 
 const MainPage = () => {
-  const [searchDrawer, setSearchModal] = useState(false);
+
+  const {searchDrawer, setSearchModal} = useContext(AuthContext);
+  
   return (
     <>
     <Navbar/>
+    <div className="main">
+
+  
     {/* <div className='main'> */}
     <MyChats/>
     {searchDrawer && <SearchUser/>}
+
     <ChatBox/>
+    </div>
     {/* </div> */}
     </>
   )

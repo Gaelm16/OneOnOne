be341d551537418fc5c4788a5b8b0526faebaedc
Gaelm16 <sandro@Sandros-iMac.com>
2022-12-23 23:43:03 -0500

    map search results

--- client/src/Components/Main/SearchUser.js
+++ client/src/Components/Main/SearchUser.js
@@ -1,32 +1,47 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
+import React, {useContext, useEffect, useState} from 'react';
+import axios from 'axios';
+import { AuthContext } from '../../UserContext';
 
 const SearchUser = () => {
     const [searchInput, setsearchInput] = useState("");
     const [searchResult, setsearchResult] = useState([]);
 
+    const {setSearchModal} = useContext(AuthContext);
+
     const fetchSearchedUser = async () => {
         try{
-            const data = axios.get(`http://localhost:4000/search?=${input}`);
+            const { data } = await axios.get(`http://localhost:4000/?search=${searchInput}`);
             setsearchResult(data);
         } catch(e){
             console.log(e);
         }
     }
 
-    
   return (
     <> 
-        <div>
-            <h1>SearchUser</h1>
-            <input 
-            type="text" 
-            placeholder="search user..."
-            value={searchInput}
-            onChange={(e) => setsearchInput(e.target.value)}
-            />
-            <button className='button'>Search</button>
+        <div className='SearchModal'>
+            <h3>SearchUser</h3>
+            <div className="inline">
+                <input 
+                type="text" 
+                placeholder="search user..."
+                value={searchInput}
+                onChange={(e) => setsearchInput(e.target.value)}
+                />
+                <button className='button' onClick={fetchSearchedUser}>Search</button>
+                <button className='button' onClick={() => setSearchModal(false)}> Cancel </button>
+            </div>
         </div>
+        {searchResult.map((userData) => {
+            console.log(userData)
+            return (
+                <div key={userData._id} className="myChats-tab"> 
+                    <h2 >{userData.userName}</h2>
+                    <button className="button">Start Chat</button>
+                </div>
+            )
+               
+        })}
     </>
   )
 }

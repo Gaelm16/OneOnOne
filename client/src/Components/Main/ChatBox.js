6adb5259551cf6678ed3a405c5d522aa9f3a443d
Gaelm16 <sandro@Sandros-iMac.com>
2022-12-29 22:23:21 -0500

    cleanup file

--- client/src/Components/Main/ChatBox.js
+++ client/src/Components/Main/ChatBox.js
@@ -4,7 +4,6 @@ import SingleChat from './SingleChat'
 const ChatBox = () => {
   return (
     <section className='Box'>
-        {/* <SingleChat></SingleChat> */}
         <SingleChat/>
     </section>
   )

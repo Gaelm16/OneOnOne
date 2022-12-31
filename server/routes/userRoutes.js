c1013d8e96f43fca7a27796598d11b1b2356a39b
Gaelm16 <sandro@Sandros-iMac.com>
2022-12-30 21:48:14 -0500

    removed get route for userInfo

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const {registerUser, loginUser, getloggedIn, logout, searchUser, getUserInfo} = require('../controllers/usercontroller')
+const {registerUser, loginUser, getloggedIn, logout, searchUser} = require('../controllers/usercontroller')
 const {authenticateUser} = require('../middleware/authenticate')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/loggedIn', getloggedIn)
 router.get('/logout', logout)
-router.get('/userInfo', getUserInfo)
 router.get('/', authenticateUser, searchUser)
 
 module.exports = router

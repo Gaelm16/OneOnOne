827c2cc20d78c1b28c79c16d829be971901ea003
Gaelm16 <sandro@Sandros-iMac.com>
2023-01-02 23:27:02 -0500

    changed post route

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -6,6 +6,6 @@ const {sendMessage, getAllMessages} = require('../controllers/messageControllers
 
 //router.post('/', authenticateUser, sendMessage)
 router.route('/').post(authenticateUser, sendMessage);
-router.get('/:chatId', authenticateUser, getAllMessages);
+router.route('/:chatId').get(authenticateUser, getAllMessages);
 
 module.exports = router;

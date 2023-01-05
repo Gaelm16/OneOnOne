39457e664e0457c0ca3cf6924127dc67821bebea
Gaelm16 <sandro@Sandros-iMac.com>
2023-01-04 22:23:26 -0500

    updated app name to OneOnOne

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
                      <> 
                     <div className="nav-header">
                         <Link to='/posts' className='links-btn'>
-                            <h1>WeShare</h1>
+                            <h1>OneOnOne</h1>
                         </Link>
                     </div>
                    <ul className='nav-links'>
